5d84f3db71c76efccb3eea4309bc539978ca7374
Paul Tetreau <paul@blacksheepwall.com>
2015-01-20 15:20:49 -0500

    rust updates broked it all, still wip, zero functionality atm

--- src/htma.rs
+++ src/htma.rs
@@ -54,18 +54,19 @@ mod htmacp
     err = unsafe { libc::funcs::posix88::unistd::setuid(99) };
     if (err != 0) { panic!("could not change user: {}", std::os::errno()); }
 
-    err = unsafe { libc::funcs::posix88::unistd::chdir("/var/htma".to_c_str().as_ptr()) };
+    err = unsafe { libc::funcs::posix88::unistd::chdir(std::ffi::CString::from_slice("/var/htma".as_bytes()).as_ptr()) };
     if (err != 0) { panic!("could not change directory to /var/htma: {}", std::os::errno()); }
   } 
 
   fn rep(mut connection: std::io::TcpStream, eval: fn (&str) -> String)
   {
-    let mut buffer = [0u8, ..1024];
+    //let mut buffer = [0u8, ..1024];
+    let mut buffer = [0];
     connection.read(&mut buffer);
 
     let buffer_str = match std::str::from_utf8(&mut buffer) {
-        Some(s) => s,
-        None => "",
+        Ok(s) => s,
+        Err(e) => "",
       };
 
     connection.write_str(eval(buffer_str).as_slice());
@@ -77,8 +78,8 @@ mod htmacp
     let listener = std::io::TcpListener::bind(format!("{}:{}", ip, port).as_slice())
       .ok()
       .expect("failed to bind");
-    //change directory and drop privileges
-    mic_drop();
+    // change directory and drop privileges
+    //mic_drop();
     let mut acceptor = listener.listen()
       .ok()
       .expect("failed to listen");
@@ -105,7 +106,7 @@ mod htma
 
   struct Memory
   {
-    size: uint,
+    size: usize,
     string: String,
     pointer: *const u8,
   }
@@ -113,7 +114,7 @@ mod htma
   struct Memory_slice
   {
     pointer: *const u8,
-    size: uint,
+    size: usize,
   }
 
   enum URIState
@@ -160,7 +161,8 @@ mod htma
       }
     }
 
-    let maybe_num = from_str(size_str.as_slice());
+    // let maybe_num = std::from_str::<usize>(size_str.as_slice());
+    let maybe_num = size_str.parse::<usize>();
     match maybe_num
     {
       Some(number) => memory.size = number,
@@ -173,7 +175,6 @@ mod htma
   fn add_headers(body: &str)
   -> String
   {
-    //println!("HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: {}\n{}", body.len(), body)
     format!("HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: {}\n\n{}\n\n", body.len(), body)
   }
 }
@@ -188,9 +189,13 @@ mod dma
   use std::num::Int;
 
   //takes an address and returns the data as a hex encoded string
-  pub fn read_memory_pointer(memory_address: *const u8, memory_size: uint)
+  pub fn read_memory_pointer(memory_address: *const u8, memory_size: usize)
   -> String
   {
+    // print what we're doing
+    println!("/{:p}/{} -> ", memory_address, memory_size);
+
+
     //try to mprotect the page, mprotect will fail if we aren't allowed to access that memory
     //(as read/write/execute)
     let mprotect_result = unsafe
@@ -208,7 +213,7 @@ mod dma
     }
     else if(0 != memory_size)
     {
-      println!("grabbing {} bytes from {}", memory_size, memory_address);
+      println!("grabbing {} bytes from {:p}", memory_size, memory_address);
       //std::io::timer::sleep(std::time::duration::Duration::seconds(5));
 
       http_string = format!("");
@@ -216,7 +221,7 @@ mod dma
       for i in std::iter::range(0, memory_size)
       {
         //get the byte
-        let byte = unsafe { *(((memory_address as uint) + i) as *const u8) };
+        let byte = unsafe { *(((memory_address as usize) + i) as *const u8) };
         //high byte
         http_string.push(u8_to_hex(byte >> 4));
         //low byte
@@ -230,56 +235,56 @@ mod dma
   pub fn get_memory_pointer(encoded_memory_address: &str)
   -> *const u8
   {
-    let decoded_memory_address = hex_str_to_uint(encoded_memory_address);
+    let decoded_memory_address = hex_str_to_usize(encoded_memory_address);
     let p: *const u8 = decoded_memory_address as *const u8;
 
     p
   }
 
   // because nothing stable can do hex >.<
-  pub fn hex_str_to_uint(hex_str: &str)
-  -> uint
+  pub fn hex_str_to_usize(hex_str: &str)
+  -> usize
   {
-    let mut ret_uint = 0;
+    let mut ret_usize = 0;
     let mut sig = hex_str.len();
     let mut trailing_zero = false;
 
     for c in hex_str.chars()
     {
-      ret_uint += ((hex_byte_to_u8(c) as uint) << 4*(sig-1));
+      ret_usize += ((hex_byte_to_u8(c) as usize) << 4*(sig-1));
 
       sig -= 1;
     }
 
-    ret_uint
+    ret_usize
   }
 
   pub fn hex_byte_to_u8(c: char)
   -> u8
   {
-    let mut ret_uint: u8 = 0;
+    let mut ret_usize: u8 = 0;
     match c
     {
-      '0' => { ret_uint = 0 },
-      '1' => { ret_uint = 1 },
-      '2' => { ret_uint = 2 },
-      '3' => { ret_uint = 3 },
-      '4' => { ret_uint = 4 },
-      '5' => { ret_uint = 5 },
-      '6' => { ret_uint = 6 },
-      '7' => { ret_uint = 7 },
-      '8' => { ret_uint = 8 },
-      '9' => { ret_uint = 9 },
-      'a' => { ret_uint = 10 },
-      'b' => { ret_uint = 11 },
-      'c' => { ret_uint = 12 },
-      'd' => { ret_uint = 13 },
-      'e' => { ret_uint = 14 },
-      'f' => { ret_uint = 15 },
-      _   => { ret_uint = 0; }, // we're 0x compatible!
+      '0' => { ret_usize = 0 },
+      '1' => { ret_usize = 1 },
+      '2' => { ret_usize = 2 },
+      '3' => { ret_usize = 3 },
+      '4' => { ret_usize = 4 },
+      '5' => { ret_usize = 5 },
+      '6' => { ret_usize = 6 },
+      '7' => { ret_usize = 7 },
+      '8' => { ret_usize = 8 },
+      '9' => { ret_usize = 9 },
+      'a' => { ret_usize = 10 },
+      'b' => { ret_usize = 11 },
+      'c' => { ret_usize = 12 },
+      'd' => { ret_usize = 13 },
+      'e' => { ret_usize = 14 },
+      'f' => { ret_usize = 15 },
+      _   => { ret_usize = 0; }, // we're 0x compatible!
     }
 
-    ret_uint
+    ret_usize
   }
   pub fn u8_to_hex(byte: u8)
   -> char
